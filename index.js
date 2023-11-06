b57f42b6dcbb4ee4f5d7bd0dccd23d4edc66442c
Arsen Hryhoriak <136361705+Lifuss@users.noreply.github.com>
2023-11-06 13:19:24 +0200

    commander refactor

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const contacts = require("./contacts.js");
+
 // yargs
-const yargs = require("yargs");
-const { hideBin } = require("yargs/helpers");
+// const yargs = require("yargs");
+// const { hideBin } = require("yargs/helpers");
 
 // commander
+const { program } = require("commander");
+
+// створю набір опції по типу option('-прапор, його повне ім'я <type це дженерік>', "опис"(опціонально), дефолтне значення(опціонально))
+program
+  .option("-a, --action <type>", "chose action")
+  .option("-i --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+// парсим рядок консолі
+program.parse(process.argv);
+// передача actions в argv
+const argv = program.opts();
 
 const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
@@ -34,6 +49,7 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
 };
 
 // yargs
-const array = hideBin(process.argv);
-const { argv } = yargs(array);
+// const array = hideBin(process.argv);
+// const { argv } = yargs(array);
+
 invokeAction(argv);
